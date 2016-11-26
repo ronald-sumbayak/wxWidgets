de321d77de83f8a41daec130ab450492084a3fa4
ronald-sumbayak <ronaldsumbayak611@gmail.com>
2016-11-26 22:18:15 +0700

    AlphaNumericPrinter

--- AlphaNumericPrinter/Main.cpp
+++ AlphaNumericPrinter/Main.cpp
@@ -4,13 +4,13 @@
 
 class MyApp : public wxApp {
 public:
-    virtual bool OnInit ();
+    virtual bool init ();
 };
 
 IMPLEMENT_APP (MyApp)
 DECLARE_APP (MyApp)
 
-bool MyApp::OnInit () {
+bool MyApp::init () {
     srand (time (nullptr));
     MainFrame* frame = new MainFrame ("AlphaNumeric Printer");
     frame->Show ();
